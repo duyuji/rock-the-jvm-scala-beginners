3d914043595c63a83e206ee747d869e9213bbeff
Eduardo Custodio <eduardo.custodio@reservafacil.tur.br>
2018-11-27 20:27:40 -0200

    Tail recursion exercises completed

--- src/main/scala/lectures/part1basics/Recursion.scala
+++ src/main/scala/lectures/part1basics/Recursion.scala
@@ -25,7 +25,7 @@ object Recursion extends App {
     factHelper(n, 1)
   }
 
-  println(anotherFactorial(20000))
+//  println(anotherFactorial(20000))
 
   // WHEN YOU NEED LOOPS, USE _TAIL_ RECURSION.
 
@@ -34,4 +34,34 @@ object Recursion extends App {
     2. IsPrime function tail recursive
     3. Fibonacci function, tail recursive
    */
+
+  def concatString(n: Int, s: String): String = {
+    if (n > 1) s
+    else concatString(n - 1, s + s)
+  }
+
+  //my prime was previously recursive, in use the same
+  def primeNumber(n: Int): Boolean = {
+    def testPrime(n: Int, i: Int): Boolean = {
+      if (i == n) {
+        return true
+      } else if (n % i == 0 && (i != 1 || i != n)) {
+        return false
+      } else {
+        return testPrime(n, i + 1)
+      }
+    }
+
+    return testPrime(n, 2)
+  }
+
+  def fibonacci(n: Int): Int = {
+    def fibonacciHelper(n: Int, i: Int, accumulator1: Int, accumulator2: Int): Int =
+      if (n <= i) accumulator1 + accumulator2
+      else fibonacciHelper(n, i + 1, accumulator2, accumulator1 + accumulator2)
+
+    fibonacciHelper(n, 2,0, 1)
+  }
+
+  println(fibonacci(6))
 }
