60e38322dbb4d73fd522c8364eee6fdb61230123
Eduardo Custodio <du.yujixd@gmail.com>
2018-12-08 18:11:09 -0200

    Multiple constructors(default parameters is better)

--- src/main/scala/lectures/part2oop/OOBasics.scala
+++ src/main/scala/lectures/part2oop/OOBasics.scala
@@ -5,6 +5,7 @@ object OOBasics extends App {
   val person = new Person("Eduardo", 29)
   println(person.age)
   person.greet("João")
+  person.greet()
 }
 
 // constructor
@@ -14,7 +15,15 @@ class Person(name: String, val age: Int) {
 
   println(1 + 3)
 
+  // method
   def greet(name: String): Unit = println(s"${this.name} says: Hi, $name")
+
+  // overloading
+  def greet(): Unit = println(s"Hi, I am $name")
+
+  // multiple constructors
+  def this(name: String) = this(name, 0)
+  def this() = this("Eduardo")
 }
 
 // class parameters are NOT FIELDS
