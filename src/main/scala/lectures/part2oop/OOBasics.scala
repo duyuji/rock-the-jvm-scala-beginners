b6e4d1e4861f1ed932ff230abf190664323c2b62
Eduardo Custodio <eduardo.custodio@reservafacil.tur.br>
2018-12-13 20:43:35 -0200

    create classes for exercises

--- src/main/scala/lectures/part2oop/OOBasics.scala
+++ src/main/scala/lectures/part2oop/OOBasics.scala
@@ -1,5 +1,8 @@
 package lectures.part2oop
 
+import java.time.Year
+import java.time.temporal.TemporalAmount
+
 object OOBasics extends App {
 
   val person = new Person("Eduardo", 29)
@@ -24,6 +27,76 @@ class Person(name: String, val age: Int) {
   // multiple constructors
   def this(name: String) = this(name, 0)
   def this() = this("Eduardo")
+}
+
+/*
+  Novel and a Writer
+
+  Writer: first name, surname, year
+    - method fullname
+
+  Novel: name, year of release, author
+  - authorAge
+  - isWrittenBy(author)
+  - copy (new year of release) = new instance of Novel
+
+ */
+
+/*
+
+  Counter class
+    - receives an int value
+    - method current count
+    - method to increment/decrement => new Counter
+    - overload inc/dec to receive an amount
+
+ */
+
+class Writer(firstName: String, surname: String, val year: Int) {
+
+  def fullname() = {
+    s"$firstName $surname"
+  }
+
+}
+
+class Novel(name: String, yearOfRelease: Int, author: Writer) {
+
+  def authorAge = {
+    Year.now.getValue - author.year
+  }
+
+  def isWrittenBy = {
+    author.fullname
+  }
+
+  def copy(newYearOfRelease: Int) = {
+    new Novel(name, newYearOfRelease, author)
+  }
+
+}
+
+class Counter(counter: Int) {
+
+  def currentCounter = {
+    counter
+  }
+
+  def increment = {
+    new Counter(counter + 1)
+  }
+
+  def decrement = {
+    new Counter(counter - 1)
+  }
+
+  def increment(amount: Int) = {
+    new Counter(counter + amount)
+  }
+
+  def decrement(amount: Int) = {
+    new Counter(counter - amount)
+  }
 }
 
 // class parameters are NOT FIELDS
