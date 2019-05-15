6ce7a081c67e3c210250e613856c8a210f30c55b
Eduardo Custodio <du.yujixd@gmail.com>
2019-05-15 20:46:00 -0300

    infix a prefix notation

---
+++ src/main/scala/lectures/part2oop/MethodNotations.scala
@@ -0,0 +1,34 @@
+package lectures.part2oop
+
+object MethodNotations extends App {
+
+  class Person(val name: String, favoriteMovie: String) {
+    def likes(movie: String): Boolean = movie == favoriteMovie
+    def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def unary_! : String = s"$name, what the heck?!"
+  }
+
+  val mary = new Person("Mary", "Inceptions")
+  println(mary.likes("Inception"))
+  println(mary likes "Inception") // equivalent
+  // infix notation = operator notation (syntactic sugar)
+
+  // "operators" in Scala
+  val tom = new Person("Tom", "Fight Club")
+  println(mary + tom)
+  println(mary .+(tom))
+
+  println(1 + 2)
+  println(1.+ (2))
+
+  // ALL OPERATORS ARE METHODS.
+  // Akka actors have ! ?
+
+  //prefix notation
+  val x = -1 // equivalent with 1.unary_-
+  val y = 1.unary_-
+  // unary_ prefix only works with - + ~ !
+
+  println(!mary)
+  println(mary.unary_!)
+}
