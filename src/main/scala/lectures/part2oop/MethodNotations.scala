773c41333a1c907f47c7e08bfe50a30bb3e05cbc
Eduardo Custodio <ecustodio.ext@vr.com.br>
2019-07-30 20:02:51 -0300

    method notation exercises

--- src/main/scala/lectures/part2oop/MethodNotations.scala
+++ src/main/scala/lectures/part2oop/MethodNotations.scala
@@ -2,10 +2,18 @@ package lectures.part2oop
 
 object MethodNotations extends App {
 
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favoriteMovie
     def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
     def unary_! : String = s"$name, what the heck?!"
+    def isAlive: Boolean = true
+    def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+
+    def +(nickname: String ) = new Person(s"$name ($nickname)", favoriteMovie)
+    def  unary_+ = new Person(name, favoriteMovie, age + 1)
+    def learns(something: String): String = s"$name learns $something"
+    def learnsSacala: String = learns("Scala")
+    def apply(quantity: Int):String = s"$name watched $favoriteMovie $quantity times"
   }
 
   val mary = new Person("Mary", "Inceptions")
@@ -31,4 +39,17 @@ object MethodNotations extends App {
 
   println(!mary)
   println(mary.unary_!)
+
+  //postfix notation
+  println(mary.isAlive)
+  println(mary isAlive)
+
+  // apply
+  println(mary.apply())
+  println(mary()) //equivalent
+
+  println(mary + "the rockstar" name)
+  println(+mary age)
+  println(mary learnsSacala)
+  println(mary(2))
 }
