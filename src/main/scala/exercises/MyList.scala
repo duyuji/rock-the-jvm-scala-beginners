6b94d0c905a71b03ba3b7c5229f9224f2725b607
Eduardo Custodio <ecustodio@bionexo.com>
2020-01-06 20:23:06 -0300

    inheritance excercise - 17

---
+++ src/main/scala/exercises/MyList.scala
@@ -0,0 +1,16 @@
+package exercises
+
+abstract class MyList {
+
+  /*
+    head = first element of the list
+    tail = reminder of the list
+    isEmpty = is this list empty
+    add(int) -> new list with this element added
+    toString -> a string representation of the list
+   */
+
+  val array = Array()
+
+
+}
